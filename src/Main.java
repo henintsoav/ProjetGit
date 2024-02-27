81d227c86c8ea55721dea01005848864414a4935
SQUEEZIEEE <nielsmerieau@hotmail.fr>
2024-02-27 12:28:25 +0100

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        Contact unContact = new Contact("Julie", "Chocoblast", "julie.chocoblast@gmail.com", 9739);
+        Contact johanna = new Contact("Johanna", "Chocoblast", "johanna.chocoblast@gmail.com", 975);
+        ContactManager contactManager = new ContactManager();
+        contactManager.ajouterContact(johanna);
+        contactManager.ajouterContact(unContact);
+        contactManager.afficherContacts();
     }
 }
