11833fcc4f9b5a1070c3d2b27cea14226130cd6b
SQUEEZIEEE <nielsmerieau@hotmail.fr>
2024-02-27 12:19:55 +0100

    ajout de la classe comparaison

--- src/Comparaison.java
+++ src/Comparaison.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Comparaison {
+import java.util.Collections;
+import java.util.Comparator;
+
+import static java.lang.CharSequence.compare;
+
+public class Comparaison implements Comparator<String> {
+    private String nom;
+
+    @Override
+    public int compare(String a, String b) {
+        return a.length() - b.length();
+    }
+
+    public void trierContactsParNom() {
+
+    }
 }
