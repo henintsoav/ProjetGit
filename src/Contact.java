f8cc8f823b841ab5fcf418bfeb4696876d92e8f2
SQUEEZIEEE <nielsmerieau@hotmail.fr>
2024-02-27 12:19:23 +0100

    ajout de la classe comparaison

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Contact {
+public class Contact {
+    private String nom;
+    private String prenom;
+    private String adresseEmail;
+    private int numeroTelephone;
+
+    public Contact(String nom, String prenom, String adresseEmail, int numeroTelephone) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresseEmail = adresseEmail;
+        this.numeroTelephone = numeroTelephone;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresseEmail() {
+        return adresseEmail;
+    }
+
+    public void setAdresseEmail(String adresseEmail) {
+        this.adresseEmail = adresseEmail;
+    }
+
+    public int getNumeroTelephone() {
+        return numeroTelephone;
+    }
+
+    public void setNumeroTelephone(int numeroTelephone) {
+        this.numeroTelephone = numeroTelephone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", adresseEmail='" + adresseEmail + '\'' +
+                ", numeroTelephone=" + numeroTelephone +
+                '}';
+    }
 }
--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ContactManager {
+import java.util.ArrayList;
+
+public class ContactManager {
+    private ArrayList<Contact> monContact;
+
+    public ContactManager() {
+        monContact = new ArrayList<>();
+    }
+    public void ajouterContact( Contact contact){
+        monContact.add(contact);
+    }
+
+    public void supprimerContact(Contact contact){
+        monContact.remove(contact);
+    }
+
+    public void modifierContact(Contact contact){
+        monContact.set(1, contact);
+    }
+
+    public void afficherContacts(){
+        if (monContact.isEmpty()){
+            System.out.println("C'est le vide");
+        } else {
+            System.out.println("Voici les contacts : ");
+            for (Contact c : monContact)
+                System.out.println(c);
+        }
+    }
+
 }
+
+
