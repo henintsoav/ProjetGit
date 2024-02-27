32b3a70609ac3f3460fa41d96dd34500b8763179
unknown <kendra.quisoir@gmail.com>
2024-02-27 12:20:07 +0100

    ajout de la classe contact

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Contact {
+public class Contact {
+    private String nom;
+    private String prenom;
+    private Integer numeroTelephone;
+    private String adresseEMail;
+
+    public Contact(String nom,String prenom, Integer numeroTelephone, String adresseEMail) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numeroTelephone = numeroTelephone;
+        this.adresseEMail = adresseEMail;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public java.lang.String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Integer getNumeroTelephone() {
+        return numeroTelephone;
+    }
+
+    public void setNumeroTelephone(Integer numeroTelephone) {
+        this.numeroTelephone = numeroTelephone;
+    }
+
+    public java.lang.String getAdresseEMail() {
+        return adresseEMail;
+    }
+
+    public void setAdresseEMail(String adresseEMail) {
+        this.adresseEMail = adresseEMail;
+    }
+
+    @Override
+    public String toString() {
+        return "Nom: " + nom + ", Prénom: " + prenom + ", Numéro de téléphone: " + numeroTelephone + ", Adresse e-mail: " + adresseEMail;
+    }
 }
--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class ContactManager {
+import java.util.ArrayList;
+
+public class ContactManager {
+
+    private ArrayList<Contact> contacts;
+
+    public ContactManager() {
+        contacts = new ArrayList<>();
+    }
+
+    public void ajouterContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void supprimerContact(Contact contact) {
+        contacts.remove(contact);
+    }
+
+    public void modifierContact(Contact contact, String nouveauNom, String nouveauPrenom, Integer nouveauNumero, String nouvelleAdresseEMail) {
+        int index = contacts.indexOf(contact);
+        if (index != -1) {
+            Contact c = contacts.get(index);
+            c.setNom(nouveauNom);
+            c.setPrenom(nouveauPrenom);
+            c.setNumeroTelephone(nouveauNumero);
+            c.setAdresseEMail(nouvelleAdresseEMail);
+            contacts.set(index, c);
+        } else {
+            System.out.println("Le contact n'existe pas.");
+        }
+    }
+
+    public void afficherContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("La liste de contacts est vide.");
+        } else {
+            System.out.println("Liste des contacts :");
+            for (Contact c : contacts) {
+                System.out.println(c);
+            }
+        }
+    }
+
 }
